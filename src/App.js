d33d0e81742d0c09a700b3dc3578a462188741cc
ashishsingla <ashishsingla216@gmail.com>
2021-06-17 17:43:55 +0530

    Adding basic code to all components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import Header from './Components/Header'
+import Balance from './Components/Balance'
+import BalanceCalc from './Components/BalanceCalc'
+import HistoryList from './Components/HistoryList'
+import Transaction from './Components/Transaction'
 
 function App() {
   return (
