4b5f28b5469da6673306f512eb6c86b2678489cb
ashishsingla <ashishsingla216@gmail.com>
2021-06-17 17:26:55 +0530

    Starting Expense tracker app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Balance />
+      <BalanceCalc />
+      <HistoryList />
+      <Transaction />
+    </div>
+  );
+}
+
+export default App;
